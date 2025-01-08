525e8881d9f3a181f5c7fc6e69bf1797ff8c12ad
Arthur Zherka <arthurzherko@gmail.com>
2025-01-08 22:44:08 +0100

    update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -16,10 +14,7 @@ export default defineConfig(({ mode }) => ({
       'Content-Security-Policy': 'frame-ancestors *'
     }
   },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
