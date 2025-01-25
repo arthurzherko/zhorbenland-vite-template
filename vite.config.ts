2af32c194ff7001e40b29e4091433a7fc20e9c2f
Arthur Zherka <arthurzherko@gmail.com>
2025-01-25 03:58:35 +0100

    update vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, OPTIONS',
-      'Access-Control-Allow-Headers': '*',
-      'X-Frame-Options': 'ALLOWALL',
-      'Content-Security-Policy': 'frame-ancestors *'
-    }
-  },
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
